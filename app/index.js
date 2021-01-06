63efe55820f40ee95d5266e873868d67e359bd88
soupCodez <bhadsour12@gmail.com>
2021-01-05 20:44:16 -0500

    hotfix

--- app/index.js
+++ app/index.js
@@ -29,7 +29,7 @@ module.exports = class extends (
             this.destinationPath(process.cwd().toString())
           );
       }
-      this.npmInstall()
+      this.yarnInstall()
     });
   }
 };
